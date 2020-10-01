9a9ca5169dc7ca73b55f3c774577b7e8cd4b0a2e
Rashmi Ranjan Das <rashmirdas5@gmail.com>
2020-10-02 01:31:50 +0530

    Style ChNges

--- odia/settings.py
+++ odia/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'd)e3l499(o@&t6j468#c*3k&!j^=bkztru$!t5=ks)(-fb+-9z'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['pathapadha.pythonanywhere.com']
+ALLOWED_HOSTS = []
 
 
 # Application definition
